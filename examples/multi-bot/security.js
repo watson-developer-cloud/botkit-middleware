96404b4909ac0ac82a7c12ce3b3bb7727e3fe8e9
David Terry <dgterry@us.ibm.com>
2016-10-17 17:04:17 -0400

    verify facebook sent the message we are getting

--- examples/multi-bot/security.js
+++ examples/multi-bot/security.js
@@ -1,29 +1,41 @@
-/*
- * Verify that the callback came from Facebook. Using the App Secret from
- * the App Dashboard, we can verify the signature that is sent with each
- * callback in the x-hub-signature field, located in the header.
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
  *
- * https://developers.facebook.com/docs/graph-api/webhooks#setup
- * This code is mostly adapted from wit.ai's facebook bot example.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 var crypto = require('crypto');
 
-module.exports = function verifyRequestSignature(req, res, buf) {
+/**
+ * Verify the request came from Facebook using the approaches described here:
+ * https://developers.facebook.com/docs/graph-api/webhooks
+ */
+module.exports = function verifyFacebookSignatureHeader(req, res, buf) {
   var signature = req.headers["x-hub-signature"];
+
   if (!signature) {
     console.log("Signature absent in the request: %s", JSON.stringify(req));
   } else {
-    var elements = signature.split('=');
-    var method = elements[0];
-    var signatureHash = elements[1];
-    var expectedHash = crypto.createHmac('sha1', process.env.FB_APP_SECRET)
+    // Get the facebook signature
+    var elements = signature.split('sha1=');
+    var facebookSignature = elements[1];
+
+    var expectedSignature = crypto.createHmac('sha1', process.env.FB_APP_SECRET)
       .update(buf)
       .digest('hex');
 
-    if (signatureHash !== expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
+    if (facebookSignature !== expectedSignature) {
+      throw new Error("Could not verify message was sent from Facebook.");
     }
   }
 }
